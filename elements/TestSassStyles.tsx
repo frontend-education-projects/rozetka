499b2ebdfe7ebd267bcbd923a2a692c07d2ca8dd
Yura <linetsky.yura@gmail.com>
2022-11-02 10:26:29 +0200

    changed color from ./elements/TestSassStyles.module.sass and from ./elements/TestSassStyles.tsx

--- elements/TestSassStyles.tsx
+++ elements/TestSassStyles.tsx
@@ -2,6 +2,6 @@ import styles from './TestSassStyles.module.sass'
 
 export const TestSassStyles = () => {
     return (
-        <div className={styles.test}>hello red text</div>
+        <div className={styles.test}>hello green text</div>
     )
 }
