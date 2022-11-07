992a11be7f68fadab03ae53d8fabb7e5833dc945
dipSyxx <113060461+dipSyxx@users.noreply.github.com>
2022-11-07 21:34:38 +0200

    feat/[RP-17] - Remove default murkup (#4)
    * added global style and global reset style,conected font-family
    
    * tested style
    
    * test
    
    * test2

--- elements/TestSassStyles.tsx
+++ elements/TestSassStyles.tsx
@@ -2,6 +2,10 @@ import styles from './TestSassStyles.module.sass'
 
 export const TestSassStyles = () => {
     return (
-        <div className={styles.test}>hello green text</div>
+        <header className={styles.header}>
+        <div className={styles.container}>
+            <div className={styles.logo}>Rozetka</div>
+        </div>
+        </header>
     )
 }
