7ed44b85eb1b2f425ca9ccb2d43cf5ef3c0818d6
Yura <linetsky.yura@gmail.com>
2022-12-14 22:42:29 +0200

    completed slider in main side

---
+++ pages/api/SliderPhotosApi.tsx
@@ -0,0 +1,17 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { SliderPhotoMassive } from "./data/SliderPhotosMassive";
+
+type Data = {};
+
+//? Функція яка на вході принімає запрос і відповідь
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  // Модернізація API
+  if (req.method === "GET") {
+    //? Результат функції
+    res.status(200).json(SliderPhotoMassive);
+  }
+}
