8901eb6bd43611c60015d535642eb96f7ffbca89
Yura <linetsky.yura@gmail.com>
2022-12-20 13:13:20 +0200

    feat: added dynamic slider

--- src/elements/MainSlider/MainSlider.tsx
+++ src/elements/MainSlider/MainSlider.tsx
@@ -1,21 +1,15 @@
 import 'swiper/css'
 import 'swiper/css/navigation'
 
-import Image from 'next/image'
-import Link from 'next/link'
-import React from 'react'
+import React, { ReactNode } from 'react'
 import { Autoplay, Navigation } from 'swiper'
-import { Swiper, SwiperSlide } from 'swiper/react'
+import { Swiper } from 'swiper/react'
 
-import { SliderPhotoMassive } from '../../pages/api/data/SliderPhotosMassive'
-
-type SliderPhotoProp = {
-  id: number
-  picture: string
-  pathPicture: string
+type SliderChildrenProps = {
+  children: ReactNode
 }
 
-export const MainSlider = () => {
+export const MainSlider = ({ children }: SliderChildrenProps) => {
   return (
     <>
       <Swiper
@@ -28,20 +22,7 @@ export const MainSlider = () => {
         rewind={true}
         speed={1000}
       >
-        {SliderPhotoMassive.map(({ id, picture, pathPicture }: SliderPhotoProp) => (
-          <SwiperSlide key={id}>
-            <Link href={`${pathPicture}`}>
-              <Image
-                alt="slide"
-                height={400}
-                placeholder="empty"
-                priority={true}
-                src={`/sliderImg/${picture}.jpg`}
-                width={1200}
-              />
-            </Link>
-          </SwiperSlide>
-        ))}
+        {children}
       </Swiper>
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,20 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 import { MainSlider } from 'src/elements/MainSlider/MainSlider'
 import { SideBar } from 'src/elements/SideBar/SideBar'
+import { SwiperSlide } from 'swiper/react'
 
 import { sidebarLinks } from './api/data/sidebarCategoiesMassive'
+import { SliderPhotoMassive } from './api/data/SliderPhotosMassive'
 import styles from './index.module.sass'
+
+type SliderPhotoProp = {
+  id: number
+  picture: string
+  pathPicture: string
+}
 
 export const Home = () => {
   return (
@@ -20,7 +30,22 @@ export const Home = () => {
             </div>
 
             <div className={styles.main_content}>
-              <MainSlider />
+              <MainSlider>
+                {SliderPhotoMassive.map(({ id, picture, pathPicture }: SliderPhotoProp) => (
+                  <SwiperSlide key={id}>
+                    <Link href={`${pathPicture}`}>
+                      <Image
+                        alt="slide"
+                        height={400}
+                        placeholder="empty"
+                        priority={true}
+                        src={`/sliderImg/${picture}.jpg`}
+                        width={1200}
+                      />
+                    </Link>
+                  </SwiperSlide>
+                ))}
+              </MainSlider>
             </div>
           </div>
         </div>
