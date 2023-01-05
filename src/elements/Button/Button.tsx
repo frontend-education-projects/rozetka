1cf15944a319a118e4fd6678df2122df3571febb
Yura <linetsky.yura@gmail.com>
2023-01-05 23:44:57 +0200

    feat:[RP-21] - Button with variants *demo version*

--- src/elements/Button/Button.tsx
+++ src/elements/Button/Button.tsx
@@ -4,18 +4,31 @@ import { createElement, ElementType, FC, ReactNode } from 'react'
 type ButtonProps = {
   children: ReactNode
   component: ElementType
-  variant?: 'text' | 'contained' | 'outlined'
+  variant?: 'green' | 'linkButton' | 'outlined'
   classVariant: string
   href?: string
   onClick?: () => void
+  type?: 'submit'
+  disabled?: 'isValid' | boolean
 }
 
-export const Button: FC<ButtonProps> = ({ children, component, variant, classVariant, href, onClick }) => {
+export const Button: FC<ButtonProps> = ({
+  children,
+  component,
+  variant,
+  classVariant,
+  href,
+  type,
+  disabled,
+  onClick,
+}) => {
   return createElement(
     component,
     {
       onClick,
       href,
+      type,
+      disabled,
       className: clsx(`${classVariant} ${variant || ''}`),
     },
     children,
